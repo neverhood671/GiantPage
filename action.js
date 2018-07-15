a9044e4cff14baee07403a6680aad7594fee2c0c
neverhood671 <nastya-art@yandex.ru>
2018-07-15 22:28:04 +0300

    add responsible navigation block, add slider

--- action.js
+++ action.js
@@ -3,82 +3,73 @@ var cardStyles = [];
 var currentFirstScreenshort = 0;
 
 function initPage() {
-  createScreenshotsScroll(visibleScreenshotsCount);
-  addEventListeners();
+    addEventListeners();
 }
 
 function addEventListeners() {
-  addMouseoverMousoutEventListeners(".social_links", "social_links_hover");
-  addMouseoverMousoutEventListeners(".download_button_big", "download_button_hover");
-  addMouseoverMousoutEventListeners(".arrow_background", "arrow_background_hover");
-  addMouseoverMousoutEventListeners(".testimonial", "testimonial_hover");
-  addMouseoverMousoutEventListeners(".pricing_card", "pricing_card_hover");
-  addArrowClickEventListener();
+    addMouseoverMousoutEventListeners(".social_links", "social_links_hover");
+    addMouseoverMousoutEventListeners(".download_button_big", "download_button_hover");
+    addMouseoverMousoutEventListeners(".arrow_background", "arrow_background_hover");
+    addMouseoverMousoutEventListeners(".testimonial", "testimonial_hover");
+    addMouseoverMousoutEventListeners(".pricing_card", "pricing_card_hover");
+
+    addOnClickEventListeners(".navigation", "show_nav_dropdown");
+    addScroll();
 }
 
 function addMouseoverMousoutEventListeners(targetElementsSelector, onMouseoverMousoutClassName) {
-  Array.from(document.querySelectorAll(targetElementsSelector)).forEach(elem => {
-    elem.addEventListener("mouseover", e => {
-      e.currentTarget.classList.add(onMouseoverMousoutClassName);
-    }, true);
-  });
+    Array.from(document.querySelectorAll(targetElementsSelector)).forEach(elem => {
+        elem.addEventListener("mouseover", e => {
+            e.currentTarget.classList.add(onMouseoverMousoutClassName);
+        }, true);
+    });
 
-  Array.from(document.querySelectorAll(targetElementsSelector)).forEach(elem => {
-    elem.addEventListener("mouseout", e => {
-      e.currentTarget.classList.remove(onMouseoverMousoutClassName);
-    }, true);
-  });
+    Array.from(document.querySelectorAll(targetElementsSelector)).forEach(elem => {
+        elem.addEventListener("mouseout", e => {
+            e.currentTarget.classList.remove(onMouseoverMousoutClassName);
+        }, true);
+    });
 }
 
-function addArrowClickEventListener() {
-  Array.from(document.querySelectorAll(".screenshots_block .arrow_background")).forEach(arrow => {
-    arrow.addEventListener("click", scrollScreenshots, true);
-  });
+function addOnClickEventListeners(targetElementsSelector, onClickClassName) {
+    Array.from(document.querySelectorAll(targetElementsSelector)).forEach(elem => {
+        elem.addEventListener("click", e => {
+            if (e.currentTarget.classList.contains(onClickClassName)) {
+                e.currentTarget.classList.remove(onClickClassName);
+            } else {
+                e.currentTarget.classList.add(onClickClassName);
+            }
+        }, true);
+    });
 }
 
-function scrollScreenshots(e) {
-  if (e.currentTarget.id === "left_scroll") {
-    currentFirstScreenshort = (currentFirstScreenshort === (visibleScreenshotsCount - 1)) ? 0 : currentFirstScreenshort + 1;
-  } else if (e.currentTarget.id === "right_scroll") {
-    currentFirstScreenshort = (currentFirstScreenshort === 0) ? 6 : currentFirstScreenshort - 1;
-  }
+function addScroll() {
+    let allCards = Array.from(document.querySelectorAll(".testimonial"));
+    Array.from(document.querySelectorAll(".testimonials_block .arrow_background")).forEach(elem => {
+        elem.addEventListener("click", e => {
 
-  var pictures = Array.from(document.querySelectorAll(".screenshot"));
-  var j = 0,
-    currentIndex = currentFirstScreenshort;
-  while (j < visibleScreenshotsCount) {
-    var currentIndex = (currentIndex === (visibleScreenshotsCount - 1)) ? 0 : currentIndex + 1;
-    pictures[currentIndex].setAttribute("style", cardStyles[j]);
-    j++;
-  }
+            let currentTranslate = getComputedTranslateX(allCards[0]),
+                cardWidth = parseInt(allCards[0].clientWidth),
+                cardRightMargin = parseInt(getComputedStyle(allCards[0]).marginRight),
+                shift = cardWidth + cardRightMargin,
+                shiftDirection = e.currentTarget.classList.contains("left_arrow_background") ? -1 : 1,
+                maxShift = (allCards.length - 1) * shift,
+                conclusionShift = Math.abs(currentTranslate) >= maxShift ?
+                0 : currentTranslate + shiftDirection * shift;
+
+            allCards.forEach(card => {
+                card.style.transform = `translateX(${conclusionShift}px)`;
+            });
+
+        }, true);
+    });
 }
 
+function getComputedTranslateX(obj) {
+    if (!window.getComputedStyle) return;
+    var style = getComputedStyle(obj),
+        transform = style.transform || style.webkitTransform || style.mozTransform;
 
-function createScreenshotsScroll() {
-  var pictures = Array.from(document.querySelectorAll(".screenshot"));
-  var centerPicture = Math.ceil(pictures.length / 2) - 1;
-  var centerPictureWidth = pictures[centerPicture].width,
-    centerPictureHeight = pictures[centerPicture].height;
-
-  for (var i = 0; i <= centerPicture; i++) {
-    var leftShift,
-        rightShift,
-        scale = 1;
-
-    if (i !== centerPicture) {
-      leftShift = Math.abs(i - centerPicture) * (-centerPictureWidth / 2);
-      rightShift = Math.abs(i - centerPicture) * (centerPictureWidth / 2);
-      scale = 1 - Math.abs(i - centerPicture) * 0.1;
-
-      cardStyles[i] = "left: calc( 50% + " + rightShift + "px); height:" + (centerPictureHeight * scale) + "px; z-index:" + i + ";";
-      cardStyles[2 * centerPicture - i] = "left: calc( 50% + " + leftShift + "px); height:" + (centerPictureHeight * scale) + "px; z-index:" + i + ";";
-    } else {
-      cardStyles[i] = "left: 50%; height:" + centerPictureHeight + "px; z-index:" + i + ";";
-    }
-
-  }
-
-  pictures.forEach((pic, i) => {
-    pic.setAttribute("style", cardStyles[i]);
-  });
+    mat = transform.match(/^matrix\((.+)\)$/);
+    return mat ? parseFloat(mat[1].split(', ')[4]) : 0;
 }
